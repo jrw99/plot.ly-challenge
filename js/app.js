8abea9f690916bdcfb13188a7bfc199bbca00a4f
Jon Wood <jonrwood99@gmail.com>
2021-11-20 09:48:53 -0500

    Cleaned up to put repetative code in a function and auto select the first value to plot

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 // Global to share the filtered sample
 let samples = [];
 
-// OnChange function for select. Takes in the Test Subject ID to fitler on
-function optionChanged(otuId) {   
+function getData(otuId) {
     let sample = samples.filter(sample => sample.id === otuId);
     let vals = sample[0].sample_values.slice(0,10).reverse();
     let lbls = sample[0].otu_ids.slice(0,10).reverse();
     lbls = lbls.map(lbl => "OTU " + lbl);
     let txt = sample[0].otu_labels.slice(0,10).reverse();    
 
-    let newTop10OTU = 
+    let Top10OTU = 
          [{           
              x: vals,
              y: lbls,
@@ -18,11 +17,22 @@ function optionChanged(otuId) {
              orientation: 'h'
          }];       
     
-     var layout = {
+     let lyout = {
         title: `Top 10 OTUs for Test Subject ID:${sample[0].id}`                      
      };
 
-     Plotly.react('bar', newTop10OTU, layout); 
+     let data = {
+         trace:Top10OTU,
+         layout:lyout
+     }
+
+     return data;
+}
+
+// OnChange function for select. Takes in the Test Subject ID to fitler on
+function optionChanged(otuId) {  
+    let data = getData(otuId);
+    Plotly.react('bar', data.trace, data.layout); 
 }
 
 // Load the data
@@ -39,33 +49,12 @@ d3.json("data/samples.json")
     .enter()
     .append('option')
     .text(function (d) { return d;})
-    .attr('value', function (d) {return d;});
+    .attr('value', function (d) {return d;});    
 
-
-    // Kick-off by pre-filter on the first value
-    let sample = samples.filter(sample => sample.id === '940'); 
-    
-    // The data is sorted in descending order by the samples_values so we just need 
-    // to splice the first 10 from each set within, but still need to call reverse
-    let vals = sample[0].sample_values.slice(0,10).reverse();
-    let lbls = sample[0].otu_ids.slice(0,10).reverse();
-    lbls = lbls.map(lbl => "OTU " + lbl);
-    let txt = sample[0].otu_labels.slice(0,10).reverse();
-
-    let top10OTU = 
-         [{           
-             x: vals,
-             y: lbls,
-             text: txt, 
-             type: 'bar',
-             orientation: 'h'
-         }];       
-    
-     var layout = {
-         title: `Top 10 OTUs for Test Subject ID:${sample[0].id}`                 
-     };
-
-     Plotly.newPlot('bar', top10OTU, layout);    
+    // Kick-off by pre-filtering on the first value in the select list and plot it. 
+    // Can just get the first value of the names array to accomplish...
+    let plotdata = getData(data.names[0]);
+     Plotly.newPlot('bar', plotdata.trace, plotdata.layout);    
 });
 
 
