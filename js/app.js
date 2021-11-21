28e87b72c6ad37aa1fd2ef049a3f102e9ab5ca7f
Jon Wood <jonrwood99@gmail.com>
2021-11-20 20:54:44 -0500

    Bonus inlcuded

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ function getBarData(otuId) {
          }];       
     
      let lyout = {
-        title: `Top 10 OTUs for Test Subject ID:${sample[0].id}`                      
+        title: `<b>Top 10 OTUs for Test Subject ID:${sample[0].id}</b>`                      
      };
 
      let data = {
@@ -53,7 +53,7 @@ function getBubbleData(otuId) {
          }];       
     
      let lyout = {
-        title: `Bubble Chart for Test Subject ID:${sample[0].id}`                      
+        title: `<b>Bubble Chart for Test Subject ID:${sample[0].id}</b>`                      
      };
 
      let data = {
@@ -70,6 +70,114 @@ function getMetaData(otuId) {
     return demoInfo[0];
 }
 
+// BONUS - function to return gauge data
+function getGaugeData(otuId) {
+    let demoInfo = getMetaData(otuId);  
+    
+    // pie chart converted to gauge chart
+    let gaugeData = {
+        type: 'pie',        
+        hole: 0.5,
+        rotation: 90,
+        direction: 'clockwise',
+        values: [180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180/9, 180],
+        text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],        
+        textinfo: 'text',
+        textposition: 'inside',
+        marker: {
+            colors: ['#F8F3EC','#F4F1E5','#E9E6CA','#E2E4B1','#D5E49D','#B7CC92','#8CBF88','#8ABB8F','#85B48A','transparent'],
+            labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9','']              
+        },
+        hoverinfo: "skip",
+        showlegend: false
+    }
+
+    // the dot for the needle 
+    let needleDot = {
+        type: 'scatter', // this is the smoothest animation for the needle movement
+        x: [0],
+        y: [0],
+        marker: {
+            size: 20,
+            color:'#850000'
+        },
+        hoverinfo: "skip",
+        showlegend: false        
+    }
+
+     // offsets for the degree calculation to update the needle
+    let offset = 0;  
+    switch(demoInfo.wfreq) 
+    {
+        case 2:
+        case 3:
+            offset = 3;
+            break;
+        case 4:
+            offset = 1;
+            break;
+        case 5:
+            offset = -.5;
+            break;
+        case 6:
+            offset = -2;
+            break;
+        case 7:
+            offset = -3;
+            break;
+        default:
+            offset = 0;
+    }   
+
+    // build the triangle needle
+    let degrees = 180-(20 * demoInfo.wfreq + offset); // 20 degrees for each gauge section (9 in total)
+    let radius = .5;
+    let radians = degrees * Math.PI / 180;
+
+    let x1 = 0.025 * Math.cos((radians) * Math.PI / 180);
+    let y1 = 0.025 * Math.sin((radians) * Math.PI / 180);
+    let x2 = -0.025 * Math.cos((radians) * Math.PI / 180);
+    let y2 = -0.025 * Math.sin((radians) * Math.PI / 180);
+    let x3 = radius * Math.cos(radians);
+    let y3 = radius * Math.sin(radians);
+
+    // build the SVG path string (m = moveto, L = linetos, z = closepath) 
+    let pth = 'M ' + x1 + ' ' + y1 + ' L ' + x2 + ' ' + y2 + ' L ' + x3 + ' ' + y3 + ' Z';
+
+    // layout
+    let gaugeLayout = {
+        title: `<b>Belly Button Washing Frequency</b><br>Scrubs per Week for <b>Test Subject ID:${otuId}</b>`,
+        shapes:[{
+            type: 'path',
+            path: pth,
+            fillcolor: '#850000',
+            line: {
+                color: '#850000'
+            }
+        }],
+        xaxis: {
+            zeroline:false, 
+            showticklabels:false,
+            showgrid: false, 
+            range: [-1, 1]            
+        },
+        yaxis: {
+            zeroline:false, 
+            showticklabels:false,
+            showgrid: false, 
+            range: [-1, 1]           
+        }
+    };
+  
+    let data = {
+        trace:[gaugeData, needleDot],
+        layout:gaugeLayout
+    }
+
+    return data;
+}
+
+//function to update the Demographic Info panel
 function updateDemographicData(demoData)
 {  
     demoDataDiv = d3.select('#sample-metadata');
@@ -97,7 +205,10 @@ function optionChanged(otuId) {
     Plotly.react('bubble', bubblePlotData.trace, bubblePlotData.layout); 
 
     let metaInfo = getMetaData(otuId);
-    updateDemographicData(metaInfo);    
+    updateDemographicData(metaInfo); 
+    
+    let gaugePlotData = getGaugeData(otuId);
+    Plotly.react('gauge', gaugePlotData.trace, gaugePlotData.layout); 
 }
 
 // Load the data
@@ -129,6 +240,6 @@ d3.json("data/samples.json")
     let metaInfo = getMetaData(data.names[0]);
     updateDemographicData(metaInfo);
 
+    let gaugePlotData = getGaugeData(data.names[0]);
+    Plotly.newPlot('gauge', gaugePlotData.trace, gaugePlotData.layout);   
 });
-
-
